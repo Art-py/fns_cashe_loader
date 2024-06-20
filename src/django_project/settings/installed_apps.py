89588d5f40f5dab6ebb92158aa46b04bd0d52a99
dmitry <z.dima.zero@gmail.com>
2024-06-20 13:09:20 +0300

    First commit

---
+++ src/django_project/settings/installed_apps.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Apps
+    "apps.xml_transit",
+    # Third-party apps
+]
---
+++ src/django_project/settings/storage.py
@@ -0,0 +1,8 @@
+STATIC_URL = config["storage"]["static_url"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = config["storage"]["media_url"]
+MEDIA_ROOT = config["storage"]["media_root"]
