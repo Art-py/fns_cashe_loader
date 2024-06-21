6978097158ee0b66df879274ef9f7cc3c0d6c6f9
dmitry <z.dima.zero@gmail.com>
2024-06-21 14:36:53 +0300

    Utilized black and isort

--- src/services/celery_tasks/xml_transit.py
+++ src/services/celery_tasks/xml_transit.py
@@ -6,4 +6,4 @@ from apps.xml_transit.services.exemple import exemple_service
 @shared_task(name="xml_transit.transit_xml_to_database")
 def transit_xml_to_database() -> None:
     exemple_service()
-    print('asd1223333333333333333333')
+    print("asd1223333333333333333333")
